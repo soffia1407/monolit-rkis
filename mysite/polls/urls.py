3f2d4aba3c13c3136f7103ac73211bfc5d5661ce
soffia1407 <sff140705@gmail.com>
2024-12-11 17:53:49 +0700

    Added URLs for new views + created a css embed file

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 
@@ -9,4 +8,9 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.user_profile, name='profile'),
+    path('profile/delete/', views.delete_profile, name='delete_profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
