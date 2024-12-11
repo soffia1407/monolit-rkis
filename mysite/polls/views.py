a4ac098fe83568ca0314caf8f46c2cf4136fae34
soffia1407 <sff140705@gmail.com>
2024-12-11 17:36:15 +0700

    added views for user registration, profile editing, and profile deletion. The HTML code for these pages

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,63 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
+from .models import Question, Choice, UserProfile
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from .forms import UserRegistrationForm, UserProfileForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'New Account Created: {username}')
+            return redirect('polls:login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'polls/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('polls:index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'polls/login.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated Successfully')
+            return redirect('polls:profile')
+    else:
+        form = UserProfileForm(instance=request.user.userprofile)
+    return render(request, 'polls/profile.html', {'form': form})
+
+
+@login_required
+def delete_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+        messages.success(request, 'Profile Deleted Successfully')
+        return redirect('polls:login')
+    return render(request, 'polls/delete_profile.html', {'user': request.user})
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'Logged Out Successfully')
+    return redirect('polls:login')
 
 
 class IndexView(generic.ListView):
